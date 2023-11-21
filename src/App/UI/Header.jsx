58087d4954343c3881772ea3616e0547d0a1b4b0
Wai Ian Vong <valenvwi@gmail.com>
2023-11-21 18:22:47 +0100

    Add font awesome package, and shopping cart logo on the header

--- src/App/UI/Header.jsx
+++ src/App/UI/Header.jsx
@@ -1,7 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
 export default function Header() {
   return (
-    <>
-      <h1>Restaurant name</h1>
-    </>
-  )
+    <div className="bg-dark text-white px-4 py-3 d-flex justify-content-between align-items-center">
+      <h3>ABC Restaurant</h3>
+      <FontAwesomeIcon
+        icon={faCartShopping}
+        style={{ color: "#ffffff", fontSize: "20px" }}
+      />
+    </div>
+  );
 }
