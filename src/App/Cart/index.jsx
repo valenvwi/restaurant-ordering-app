d4088e0d62404edb20b8aa92368165eeb5632544
Wai Ian Vong <valenvwi@gmail.com>
2023-11-22 18:04:20 +0100

    Update cartIndex styling for mobile view

--- src/App/Cart/index.jsx
+++ src/App/Cart/index.jsx
@@ -12,23 +12,30 @@ export default function Cart() {
 
   return (
     <div className="container mt-5 pt-5">
-      <h1>Your Cart</h1>
+      <h2 className="ms-2">Your Cart</h2>
       {cartItems.map((cartItem) => (
         <div className="card p-3 m-3" key={cartItem.id}>
           <div className="row">
-            <div className="col-md-4">
+            <div className="col-md-4 d-flex justify-content-center align-items-center">
               <img
                 src={cartItem.image}
                 alt={cartItem.name}
-                width="250px"
-                height="250px"
-                className="mx-2"
+                width="95%"
+                className="mx-2 square-img"
               />
             </div>
-            <div className="col-md-8 p-3 text-center">
-              <h1 className="m-3">{cartItem.name}</h1>
-              <h4>Price: {cartItem.price} CHF</h4>
-              <div className="d-flex align-text-center justify-content-center p-4">
+            <div className="col-md-8 p-3 text-center ">
+              {/* Text on medium and larger screens */}
+              <div className="mx-2 d-none d-sm-none d-md-block pb-2">
+                <h1 className="m-3">{cartItem.name}</h1>
+                <h5 className="fw-normal">Price: {cartItem.price} CHF</h5>
+              </div>
+              {/* Text on small and smaller screens */}
+              <div className="mx-2 d-sm-block d-md-none">
+                <h4 className="m-1">{cartItem.name}</h4>
+                <p>Price: {cartItem.price} CHF</p>
+              </div>
+              <div className="d-flex align-text-center justify-content-center">
                 <button
                   className="btn icon icon-minus-margin"
                   onClick={() => decreaseQuantity(cartItem.id)}
